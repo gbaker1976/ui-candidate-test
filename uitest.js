929dac5803642b8a438b8c6e3433cacada4407b9
Garrett Baker <garrett.baker@gmail.com>
2014-04-02 00:27:43 -0400

    Update uitest.js

--- uitest.js
+++ uitest.js
@@ -1,25 +1,25 @@
-     // Example unit test function
+     // 1. Example unit test function
      function divide( a, b ) {
         // To see the test pass, uncomment the following line
         //return a / b;
      }
 
-     // Write a function that takes a single argument (a string) and returns the string reversed.
+     // 2. Write a function that takes a single argument (a string) and returns the string reversed.
      function reverseString(str) {
          // FILL THIS IN
      }
 
-     // Write a function that takes an array of numbers and returns the minimum value
+     // 3. Write a function that takes an array of numbers and returns the minimum value
      function findMinValue(values) {
          // FILL THIS IN
      }
 
-     // Write a function that takes an array and returns the distinct values only (i.e. removes duplicates)
+     // 4. Write a function that takes an array and returns the distinct values only (i.e. removes duplicates)
      function findDistinctValues(values) {
          // FILL THIS IN
      }
 
-     // Write a function that logs the numbers from 1 to 100 to the console.
+     // 5. Write a function that logs the numbers from 1 to 100 to the console.
      // For multiples of three print "Fizz" instead of the number.
      // For multiples of five print "Buzz".
      // For numbers which are multiples of both three and five print "FizzBuzz".
@@ -27,7 +27,7 @@
          // FILL THIS IN
      }
 
-     // You have a master array of strings, where each element is a fruit name.
+     // 6. You have a master array of strings, where each element is a fruit name.
      // You have a second array of fruit name strings, that is a list of fruits that should be removed from the fruits specified in the master array.
      // For the purpose of the exercise, we will call the master array fruits and the second array fruitsToRemove.
      // Write the function that will remove the values contained in fruitsToRemove from the array fruits.
@@ -35,7 +35,7 @@
          // FILL THIS IN
      }
 
-     // Write a function to push either a simple value or an array of values onto a specified array.
+     // 7. Write a function to push either a simple value or an array of values onto a specified array.
      // For the purpose of the exercise, we will call the target array simply array and the stuff to push onto it (either a simple value or array) simply toPush.
      // If toPush is a simple value, it should be pushed onto array as an element.
      // If toPush is an array, all of its elements should be pushed onto array. Your solution should modify array (ie. not return a new array).
@@ -43,24 +43,24 @@
          // FILL THIS IN
      }
 
-     // Given a string, sourceStr, write some code that will split this string using comma as your delimiter, and producing an empty array if the string is empty.
+     // 8. Given a string, sourceStr, write some code that will split this string using comma as your delimiter, and producing an empty array if the string is empty.
      function splitListStrUsingComma(sourceStr) {
          // FILL THIS IN
      }
 
-     // Write a function that will take any number of arguments and return their sum
+     // 9. Write a function that will take any number of arguments and return their sum
      function sum() {
          // FILL THIS IN
      }
 
-     // Write a function that will return true if a specified string consists of only whitespace.
+     // 10. Write a function that will return true if a specified string consists of only whitespace.
      function isOnlyWhitespace(sourceStr) {
          // FILL THIS IN
      }
 
-     // write an example of a javascript closure
+     // 11. write an example of a javascript closure
 
-     // define a json object that represents a collection of people.
+     // 12. define a json object that represents a collection of people.
      // each person should have the following properties
      // - first name
      // - last name
@@ -69,30 +69,21 @@
      // - zip
      // - a collection of phone numbers (home, work, mobile)
 
+     // 13. Write an example of a factory function that returns an instance of one of three prototypes.
+     // The factory should accept a string paramenter that determines which prototype to create.
+     // Create your own constructors; name them anything you want. 
+     // They must all inherit from the same parent prototype.
+     
+     // 14. Write an example of a singleton object.
 
-     // Create a javascript object (DataTable) with the following:
-     // A private columns property (string array)
-     // A private rows property (string array)
-     // A public method addRows that adds an item to the rows array
-     // A public method addColumns that adds an item to the columns array
-     // A public method getData that returns the a json object representation of the DataTable
-     // Note: the row object should be a hash of the column name and row item value
-     // Example:
-     // .addColumns('column1', 'column2', 'column3');
-     // .addRow('value1A', 'value1B', 'value1C');
-     // .addRow('value2A', 'value2B', 'value2C');
-
-     // within div1, programatically create a
+     // 15. Within a div with an id of div1, programatically create a
      // SELECT element (with multiple items) and a button.
      // when the button is clicked write out the name and value of the selected item to the console.
 
-     // Write 5 different jQuery selectors to retrieve the
-     // sample anchor in the markup below.
-
-     // Programatically create an array with 5 items.  Create a list item for each item in the array
+     // 16. Programatically create an array with 5 items.  Create a list item for each item in the array
      // and add the list items to the unordered list with an id of "list1".
 
-     // Use javascript to add a list of checkboxes and 2 links
-     // to the div with an id of "foobar"
+     // 17. Use javascript to add a list of checkboxes and 2 links
+     // to a div with an id of "foobar"
      // When the first link is clicked, all the checkboxes should be checked (i.e. check all)
      // When the second link is clicked, all the checkboxes should be unchecked (i.e. uncheck all)
