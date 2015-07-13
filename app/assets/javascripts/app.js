3a7ea8ef077ce437ce1aeae7cc54d335247f642f
0ndre <andrelee0@gmail.com>
2015-07-13 18:11:18 -0400

    rewrite the getData function; write new tests to test for data

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,22 +1,26 @@
-function populateList() {
-  // get json
-  $.getJSON( '/data/fields.json', function(data) {
-    // setup empty array to hold json data
-    var fields = [];
-    // loop through json
-    for (var i = 0; i < data.length; i++) {
-      // add data to 'fields' array with appropriate html
-      // using the name of the field as the data-name attribute
-      // and the type as the data-type attribute
-      fields.push('<li class="available-form-fields__list-item draggable" data-name="' + data[i].name + '" data-type="' + data[i].type + '">' + data[i].name + '</li>');
+var appData = (function() {
+  return {
+    getData: function(url, callback) {
+      var data = $.getJSON(url).done(callback);
+      return data;
     }
-    // append fields to list
-    $('.js-field-list').append(fields);
-    // initialize drag
-    $('.draggable').draggable({
-      revert: 'invalid',
-      scroll: false
-    });
+  };
+}());
+appData.getData('/data/fields.json', function(jsonData) {
+  // setup empty array to hold json data
+  var fields = [];
+  // loop through json data
+  for (var i = 0; i < jsonData.length; i++) {
+    // add data to 'fields' array with appropriate html
+    // using the name of the field as the data-name attribute
+    // and the type as the data-type attribute
+    fields.push('<li class="available-form-fields__list-item draggable" data-name="' + jsonData[i].name + '" data-type="' + jsonData[i].type + '">' + jsonData[i].name + '</li>');
+  }
+  // append fields to list
+  $('.js-field-list').append(fields);
+  // initialize drag
+  $('.draggable').draggable({
+    revert: 'invalid',
+    scroll: false
   });
-}
-populateList();
+});
--- app/tests/tests.js
+++ app/tests/tests.js
@@ -1,3 +1,17 @@
-QUnit.test('Hello World', 1, function(assert) {
-  assert.ok(1 == '1', 'Passed!');
-});
+QUnit.module('Data');
+
+  QUnit.test('Looking for function: appData.getData()', 1, function(assert) {
+    assert.deepEqual(typeof appData.getData, 'function', 'Looking for appData.getData() function');
+  });
+
+  QUnit.test('Testing Data', 2, function(assert) {
+    var jsonData = [{"name": "City","type": "text"},{"name": "Email Address","type": "email"},{"name": "First Name","type": "text"},{"name": "Last Name","type": "text"},{"name": "State","type": "select"},{"name": "Street Address","type": "textarea"},{"name": "Zip Code","type": "text"}]
+
+    var done = assert.async();
+
+    appData.getData('/data/fields.json', function(data) {
+      assert.ok(data, '"data" was returned from the server');
+      assert.deepEqual(data, jsonData, 'LOOKING FOR: ' + JSON.stringify(jsonData) + ', THE RESULT WAS: ' + JSON.stringify(data));
+      done();
+    });
+  });
